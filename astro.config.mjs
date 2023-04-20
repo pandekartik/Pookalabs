a015f96a8983555b6202b3e0597b56b12b0c92cb
pandekartik <pande.kartik@gmail.com>
2023-04-20 22:32:31 +0530

    footer sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,12 @@ import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
 import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
-
 import robotsTxt from "astro-robots-txt";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://pookalabs.com',
-  integrations: [tailwind(), partytown(), image(), sitemap(), robotsTxt()]
+  integrations: [tailwind(), partytown(), image(), sitemap(), robotsTxt(), compress()]
 });
